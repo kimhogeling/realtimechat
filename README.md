fec30648f9438a001f4a071361cc6f5bc029fcd2
Kim Hogeling <kimhogeling@gmail.com>
2015-02-16 23:45:32 +0100

    added redis and cool style. called it plauderpiraten

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # realtimechat
 
-Created with NodeJS, Express and socket.io
+Created with NodeJS, Express, socket.io and redis
 
 
 # important!
 
 1. use bower to install `socket.io-client`
-2. use npm to install `express` and `socket.io`
+2. use npm to install `express`, `socket.io` and `redis`
 
 
 # Todo
 
 1. Add npm package.json
-2. Save message history in App
-3. Save message history in Redis to keep it after server restart
--- app.js
+++ app.js
@@ -1,26 +1,40 @@
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var user = 0;
-io.on('connection', function(client){
-    client.emit('messages', 'Der Chatserver sagt hallo!');
-    console.log('new user!');
-    client.on('join', function(name){
-        name = name ? name : "Nutzer" + ++user;
-        client.emit('messages', 'Neuer Nutzer: ' + name);
+var redisClient = require('redis').createClient();
+
+function storeMessage(nickname, text) {
+    var message = JSON.stringify({ nickname: nickname, text: text });
+    redisClient.lpush('messages', message, function (err, res){
+        redisClient.ltrim('messages', 0, 49);
+    });
+}
+
+io.sockets.on('connection', function (client) {
+    console.log('new connection!');
+    client.on('join', function (name) {
+        redisClient.lrange('messages', 0, -1, function(err, storedMessages){
+            storedMessages = storedMessages.reverse();
+            storedMessages.forEach(function (storedMessage) {
+                console.log(storedMessage);
+                storedMessage = JSON.parse(storedMessage);
+                client.emit("messages", storedMessage.nickname + ': "' + storedMessage.text + '"');
+            });
+        });
+        client.emit('messages', 'Server: "Hallo ' + name + '"');
         client.nickname = name;
-        client.broadcast.emit('messages', 'Neuer Nutzer: ' + name);
+        client.broadcast.emit('messages', 'Neuer Nutzer: "' + name + '"');
         console.log('joined: ' + name);
     });
-    client.on('messages', function(message){
-        client.emit('messages', client.nickname + ': ' + message);
-        client.broadcast.emit('messages', client.nickname + ': ' + message);
+    client.on('messages', function (message) {
+        client.emit('messages', 'Ich: "' + message + '"');
+        client.broadcast.emit('messages', client.nickname + ': "' + message + '"');
+        storeMessage(client.nickname, message);
     });
 });
 
-app.use("/js", express.static(__dirname + '/js'));
+app.use("/stuff", express.static(__dirname + '/stuff'));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html', {});
 });
